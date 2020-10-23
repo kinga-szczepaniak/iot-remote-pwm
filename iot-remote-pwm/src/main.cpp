10ae8eeb551e9b5f4760bec023fceac07ae9d573
Kinga Szczepaniak <kinga.sz14@gmail.com>
2020-10-23 23:24:31 +0200

    blink led

--- iot-remote-pwm/src/main.cpp
+++ iot-remote-pwm/src/main.cpp
@@ -2,8 +2,14 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(3);              // wait for a second
+  digitalWrite(2, LOW);  // turn the LED off by making the voltage LOW
+  delay(1);                     // wait for a second
 }
