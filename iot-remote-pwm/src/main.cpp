8b28ffc269852a4a42d56d8f16704ed29de5f265
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-10-24 23:51:07 +0200

    set the server

--- iot-remote-pwm/src/main.cpp
+++ iot-remote-pwm/src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
 uint8_t red = 26;
 uint8_t green = 25;
-uint8_t blue = 17; 
+uint8_t blue = 17;
+
+const char *ssid = "Solaris_24";
+const char *password = "Z1emiaChrystusa";
+
+WiFiServer server(80);
 
 void setup()
 {
@@ -13,14 +20,80 @@ void setup()
   ledcSetup(1, 600, 8); // 12 kHz PWM, 8-bit resolution
   ledcSetup(2, 600, 8);
   ledcSetup(3, 600, 8);
+
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  ledcWrite(1, 130);
-  ledcWrite(2, 100);
-  ledcWrite(3, 160);
-  delay(2000);
-  
-}
+uint32_t currentTime;
+uint32_t previousTime;
+uint32_t timeoutTime = 10000;
+String header;
 
+void loop()
+{
+
+  WiFiClient client = server.available(); // Listen for incoming clients
+
+  if (client)
+  { // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client."); // print a message out in the serial port
+    String currentLine = "";       // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime)
+    { // loop while the client's connected
+      currentTime = millis();
+      if (client.available())
+      {                         // if there's bytes to read from the client,
+        char c = client.read(); // read a byte, then
+        Serial.write(c);        // print it out the serial monitor
+        header += c;
+        if (c == '\n')
+        { // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0)
+          {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            break;
+          }
+          else
+          { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        }
+        else if (c != '\r')
+        {                   // if you got anything else but a carriage return character,
+          currentLine += c; // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
+ 
